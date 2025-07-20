6cc3bfe15db520d5fe216a91cc7ebe91cadb622c
Teja-Vvrs <tejav2527@gmail.com>
2025-07-20 22:14:23 +0530

    updated crop

--- server/routes/purchase.js
+++ server/routes/purchase.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const purchaseController = require('../controllers/purchaseController');
-const authMiddleware = require('../middleware/auth');
+// const express = require('express');
+// const router = express.Router();
+// const purchaseController = require('../controllers/purchaseController');
+// const authMiddleware = require('../middleware/auth');
 
-// Customer-only routes
-router.post('/purchase', authMiddleware(['Customer']), purchaseController.createPurchase);
-router.get('/purchases', authMiddleware(['Customer']), purchaseController.getPurchaseHistory);
+// // Customer-only routes
+// router.post('/purchase', authMiddleware(['Customer']), purchaseController.createPurchase);
+// router.get('/purchases', authMiddleware(['Customer']), purchaseController.getPurchaseHistory);
 
-module.exports = router;
+// module.exports = router;
