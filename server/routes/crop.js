6dbea402484af610731649bcd1673a7cebe1bc70
Yaswanthvijjapu <yaswanthvijjapu799@gmail.com>
2025-07-21 00:26:22 +0530

    Add nearby crops endpoint and controller for crop distance calculation

--- server/routes/crop.js
+++ server/routes/crop.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const cropController = require('../controllers/cropController');
 const authMiddleware = require('../middleware/auth');
+const nearbyCropController = require('../controllers/nearbyCropController')
 
 router.post('/crop', authMiddleware(), cropController.postCrop);
+router.get('/nearby-crops',nearbyCropController.getNearbyCrops);
+
 
 module.exports = router;   
